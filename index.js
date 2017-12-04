fc57dbc182539412a9e0448d97e5d89462be22b0
surekha <surekhasrao@gmail.com>
2017-12-04 17:44:55 +0000

    Quote Generator with facebook share

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 /*global $*/
+$(document).ready(function(){
+    function getQuote(){
+        $.ajax({
+            method: "GET",
+            url: "https://got-quotes.herokuapp.com/quotes",
+            success: function(response){
+                $('#quote').text(response.quote);
+                //document.getElementById('quote').innerHTML= response.quote; //javascript version over the jquery version
+                $('#character').text(response.character);
+            }   //document.getElementById('character').innerHTML= response.character;
+        });
+    }
+    getQuote();
+    
+    $("#getQuote").on("click", function(){
+        getQuote();
+    });
+
+})
+
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.6";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
